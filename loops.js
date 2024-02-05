9f0690703d043ab55b485ff50eafd57b8048e8b8
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-05 12:26:41 +0700

    loops with errors

--- loops.js
+++ loops.js
@@ -1,3 +1,12 @@
 // Your task is to write a JavaScript function named sumNumbers that uses a for loop to sum up all numbers from 1 to a given number n.
 // This function, should take one parameter n and return the sum of all numbers from 1 up to (and including) n.
 
+function sumNumbers(n){
+    total = 0
+    for (let i = n; n >= 0; i = i + 1){
+        total = total + i
+        return total
+    }
+}
+const testLoop = sumNumbers(5)
+console.log(testLoop);
