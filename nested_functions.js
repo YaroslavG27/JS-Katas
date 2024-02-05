11b969e01ffd7b2fe5db277a642029a2db86dbcc
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-05 19:21:21 +0700

    sum of even function added

--- nested_functions.js
+++ nested_functions.js
@@ -48,3 +48,27 @@
 // console.log(calcArea(30, 20)); // null
 // console.log(calcArea(30, 20, "cube")); // null
 
+
+// Your task is to create a JavaScript function sumEvenNumbers that processes an array of numbers.
+// For each number in the array, the function should call an external function isEven to check if the number is even.
+// Then, it should sum all the even numbers and return the total sum.
+
+// Automatic tests are not available for this kata, please use the tests below in NodeJs.
+
+function isEven(num){
+    return num % 2 === 0
+}
+
+function sumEvenNumbers(numbers){
+    let sum = 0
+    for(let num of numbers){
+        if(isEven(num)){
+            sum = sum + num
+        }
+    }
+    return sum
+}
+
+console.log(sumEvenNumbers([1, 20, 5, 4])); // 24
+console.log(sumEvenNumbers([0, 100, -20, 5.5, -10])); // 70
+console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6])); // 12
