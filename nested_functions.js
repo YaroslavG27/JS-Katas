ab93a7018fc21e30b18de11c6888b15e1c50e2c9
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-05 12:30:22 +0700

    nested functions file created

---
+++ nested_functions.js
@@ -0,0 +1,6 @@
+// Your task is to create two functions.
+// The first function, calculateSquare, should take a number as a parameter and return its square.
+// The second function, sumOfSquares, should take two numbers, invoke calculateSquare for each number, and then return the sum of their squares.
+
+// Automatic tests are not available for this kata, please use the tests below in NodeJs.
+
