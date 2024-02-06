74e07d340e9559edf4a2c898046923358d4808c8
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-06 10:47:28 +0700

    a villa object was created with a sub-property nested in one property

--- introOOP.js
+++ introOOP.js
@@ -6,3 +6,22 @@
 //     bathroom: 1,
 //     material: 'stone'
 // }
+
+// Create a villa object with the properties:
+
+// windows = 6
+// material = 'stone'
+// and a property livingroom with the following sub-properties:
+
+// windows = 2
+// doors = 1
+
+// let villa = {
+//     windows: 6,
+//     material: 'stone',
+//     livingRoom: {
+//         windows: 2,
+//         doors: 1
+//     }
+// }
+// console.log(villa)
