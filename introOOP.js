2d97fe4a88dceb42eee887c9faeaf515f4bfa55e
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-06 11:11:11 +0700

    a function that count the total of the windows in the object was created

--- introOOP.js
+++ introOOP.js
@@ -33,3 +33,23 @@
 //     speed: 90
 // }
 // console.log(team);
+
+// Create a function countWindows that counts, then returns, the total number of windows in the house parameter object, by accessing each room's windows:
+
+// let house = {
+//     livingRoom: {
+//       windows: 4
+//     },
+//     bedroom: {
+//       windows: 2
+//     },
+//     kitchen: {
+//       windows: 1
+//     }
+//   }
+
+//   function countWindows(house){
+//     return house.livingRoom.windows + house.bedroom.windows + house.kitchen.windows
+//   }
+
+//   console.log(countWindows(house));
