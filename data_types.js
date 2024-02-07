459dbb01e24e806070346472703cd89ace14041d
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 10:17:31 +0700

    uncomment the function

--- data_types.js
+++ data_types.js
@@ -3,35 +3,35 @@
 // For example, $100 discounted 25% (0.25) is $75.
 // If either parameter is not a number, the function should return the error message: "Error: Invalid input"
 
-// function addDiscount(price, discount){
-//     let total = 0
-//     if (typeof(price) === 'number' && typeof(discount) === 'number'){
-//         return total = price - (price * discount)
-//     } else {
-//         return 'Error: Invalid input'
-//     }
-// }
+function addDiscount(price, discount){
+    let total = 0
+    if (typeof(price) === 'number' && typeof(discount) === 'number'){
+        return total = price - (price * discount)
+    } else {
+        return 'Error: Invalid input'
+    }
+}
 
-// const discountTest = addDiscount('hello', 'hello')
-// console.log(discountTest);
+const discountTest = addDiscount('hello', 'hello')
+console.log(discountTest);
 
 // Create a JavaScript function named formatDisplayName that takes two parameters: a user's first name and last name.
 // If the last name is not provided, the function should return the error message: "Last name missing".
 // The function should check if both parameters are strings and then return a formatted full name with a space in between.
 // If either parameter is not a string, the function should return the error message: "Error, not a string".
 
-// function formatDisplayName(firstName, lastName){
-//     if (typeof(firstName) === 'string' && typeof(lastName) === 'string'){
-//         return firstName + ' ' + lastName
-//     } else if (!lastName){
-//         return 'Last name missing'
-//     } else {
-//         return 'Error, not a string'
-//     }
-// }
+function formatDisplayName(firstName, lastName){
+    if (typeof(firstName) === 'string' && typeof(lastName) === 'string'){
+        return firstName + ' ' + lastName
+    } else if (!lastName){
+        return 'Last name missing'
+    } else {
+        return 'Error, not a string'
+    }
+}
 
-// const displayNameTest = formatDisplayName(100, true)
-// console.log(displayNameTest);
+const displayNameTest = formatDisplayName(100, true)
+console.log(displayNameTest);
 
 // Sum Numbers in Array
 // Create a function sumNumbers that takes an array as a parameter.
