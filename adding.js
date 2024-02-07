fee1f04453eaaf45abd28cb3d952f7678a7dcbe2
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 11:35:59 +0700

    function addAges created but not working as expected

--- adding.js
+++ adding.js
@@ -2,7 +2,7 @@
 // This function should take two parameters: an object and a number.
 // The function should add the provided number to the object as age and return the updated object.
 
-let a = {'name':'Alice'} 
+/*let a = {'name':'Alice'} 
 let numA = 25
 let b = {'name':'Bob','job':'coder'} 
 let numB = 51
@@ -13,4 +13,24 @@ function addAge(object, n){
 }
 
 console.log(addAge(a, numA));
-console.log(addAge(b, numB));
+console.log(addAge(b, numB));*/
+
+// Your task is to create a JavaScript function named addAges.
+// This function should take two parameters: an array of objects and an array of numbers of equal length.
+// The function should add the numbers in the second array to the objects as age, following the corresponding index, and return the updated array of objects.
+
+let arrO1 = [{'name':'Alice'},{'name':'Bob'}]
+let arrN1 = [21,51]
+let arrO2 = [{'name':'Tom'},{'name':'Tim'},{'name':'Tam'}]
+let arrN2 = [30,31,32]
+
+function addAges(arrO, arrN){
+    
+    for (let arr of arrO){
+        arr.age = arrN[0]++
+    }
+    return arrO
+}
+
+console.table(addAges(arrO1, arrN1));
+console.table(addAges(arrO2, arrN2));
