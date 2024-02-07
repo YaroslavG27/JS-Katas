a9c14b3b41a5e09343d0181b16554c28ae646f80
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 13:42:23 +0700

    fixed some errors

--- method.js
+++ method.js
@@ -12,9 +12,9 @@ console.log(greeter.sayHello()) // 'Hello World'*/
 // Create an object greeter.
 // In the object, create a method sayHello that takes a parameter, then returns a string as per the tests below.
 
-/*let greeter = {
+let greeter = {
     sayHello: function(name){
-        if(typeof name !== 'string'){
+        if(!name){
             return 'Hello guest'
         }
         return 'Helo ' + name
@@ -23,17 +23,19 @@ console.log(greeter.sayHello()) // 'Hello World'*/
 
 console.log(greeter.sayHello('John')) // 'Hello John'
 console.log(greeter.sayHello('Alice')) // 'Hello Alice'
-console.log(greeter.sayHello())  // 'Hello guest'*/
+console.log(greeter.sayHello())  // 'Hello guest'
 
 // Create an object counter with 2 methods: increase and decrease.
 // Each method takes an object, as per the below tests, and returns the object with internal count value either increased by 1 or decreased by 1.
 
 /*let counter = {
     increase: function(object){
-        return object.count + 1
+        object.count = object.count + 1
+        return object
     },
     decrease: function(object){
-        return object.count - 1
+        object.count = object.count - 1
+        return object
     }
 }
 
@@ -60,7 +62,7 @@ console.log(counter.decrease({ count: 10 }))*/
 // calculateWaterIntake(weight)
 // Water Intake = weight * 0.033
 
-let nutrition = {
+/*let nutrition = {
     calculateCaloriesFromMacros: function(protein, carbs, fat){
         let totalCalories = (protein * 4) + (carbs * 4) + (fat * 9)
         return totalCalories
@@ -89,5 +91,5 @@ console.log(nutrition.calculateBMI(65, 155))
 console.log(nutrition.calculateBMR(80, 180, 35, 'male'))
 console.log(nutrition.calculateBMR(60, 170, 26, 'female'))
 console.log(nutrition.calculateWaterIntake(80))
-console.log(nutrition.calculateWaterIntake(55))
+console.log(nutrition.calculateWaterIntake(55))*/
  
