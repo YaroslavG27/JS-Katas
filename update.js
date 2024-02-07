02374d22dd51ba6cbb5d38f407f33f4b316dd073
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 10:15:29 +0700

    bill marked as paid

--- update.js
+++ update.js
@@ -1,2 +1,15 @@
 // Your task is to create a function called markAsPaid.
 // This function should take an object as its only parameter (a bill), update the status property of the bill to paid, and then return the updated object.
+
+let a = {"total": "105", "status": "pending"}
+// markAsPaid(a) // {"total": "105", "status": "paid"}
+let b = {"total": "33.2", "status": "pending"}
+// markAsPaid(b) // {"total": "33.2", "status": "paid"}
+
+function markAsPaid(object){
+    object.status = 'paid'
+    return object
+}
+
+console.log(markAsPaid(a))
+console.log(markAsPaid(b))
