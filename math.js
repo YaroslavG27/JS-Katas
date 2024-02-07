b0d665e0728f7366a61bf19441d9c8ee37707d60
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 14:57:05 +0700

    could figure it out, the function is in progress

---
+++ math.js
@@ -0,0 +1,12 @@
+// Create a JavaScript function called findSmallestNumber.
+// This function should take between 2 and 4 numbers as its parameters and return the smallest number.
+// Use the correct Math method.
+
+function findSmallestNumber(){
+    return Math.min()
+}
+
+
+console.log(findSmallestNumber(2,71,5,3.9))
+console.log(findSmallestNumber(20,80,-5.5))
+console.log(findSmallestNumber(8,1))
