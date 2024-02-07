609aa212571598ec89a82f3a60630cf7633f41d9
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 13:06:09 +0700

    counter created and add couple of properties

--- method.js
+++ method.js
@@ -12,7 +12,7 @@ console.log(greeter.sayHello()) // 'Hello World'*/
 // Create an object greeter.
 // In the object, create a method sayHello that takes a parameter, then returns a string as per the tests below.
 
-let greeter = {
+/*let greeter = {
     sayHello: function(name){
         if(typeof name !== 'string'){
             return 'Hello guest'
@@ -23,6 +23,23 @@ let greeter = {
 
 console.log(greeter.sayHello('John')) // 'Hello John'
 console.log(greeter.sayHello('Alice')) // 'Hello Alice'
-console.log(greeter.sayHello())  // 'Hello guest'
+console.log(greeter.sayHello())  // 'Hello guest'*/
+
+// Create an object counter with 2 methods: increase and decrease.
+// Each method takes an object, as per the below tests, and returns the object with internal count value either increased by 1 or decreased by 1.
+
+let counter = {
+    increase: function(object){
+        return object.count + 1
+    },
+    decrease: function(object){
+        return object.count - 1
+    }
+}
+
+console.log(counter.increase({ count: 1 }))
+console.log(counter.increase({ count: 10 }))
+console.log(counter.decrease({ count: 1 }))
+console.log(counter.decrease({ count: 10 }))
  
  
