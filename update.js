0a30ec4cf15a136814cd55623c42d47259b02947
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 10:06:46 +0700

    update file was creates

---
+++ update.js
@@ -0,0 +1,2 @@
+// Your task is to create a function called markAsPaid.
+// This function should take an object as its only parameter (a bill), update the status property of the bill to paid, and then return the updated object.
