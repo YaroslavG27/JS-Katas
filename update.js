aa776ec9ab99efee213dd61530c24154ad376b76
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-07 10:29:40 +0700

    function to mark as value updated

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 // Your task is to create a function called markAsPaid.
 // This function should take an object as its only parameter (a bill), update the status property of the bill to paid, and then return the updated object.
 
-let a = {"total": "105", "status": "pending"}
+/*let a = {"total": "105", "status": "pending"}
 // markAsPaid(a) // {"total": "105", "status": "paid"}
 let b = {"total": "33.2", "status": "pending"}
 // markAsPaid(b) // {"total": "33.2", "status": "paid"}
@@ -12,4 +12,21 @@ function markAsPaid(object){
 }
 
 console.log(markAsPaid(a))
-console.log(markAsPaid(b))
+console.log(markAsPaid(b))*/
+
+// Your task is to create a function called markAsValue.
+// This function should take 2 parameters: an object (a bill), and a string (a value).
+// The function should update the status property of the bill to value parameter, and then return the updated object.
+
+/*let a = {"total": "99", "status": "pending"}
+// markAsValue(a, "accepted") // {"total": "99", "status": "accepted"}
+let b = {"total": "71.5", "status": "pending"}
+// markAsValue(b, "denied") // {"total": "71.5", "status": "denied"}
+
+function markAsValue(object, state){
+    object.status = state
+    return object
+}
+
+console.log(markAsValue(a, 'accepted'));
+console.log(markAsValue(b, 'denied'));*/
