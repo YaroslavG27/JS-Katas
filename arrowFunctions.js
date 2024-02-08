319c1591dad25fc34fb131f0d3cd3e7aaeeab25d
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 12:00:57 +0700

    simplify check positive function

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -18,7 +18,15 @@ console.log(multiply(9,3))*/
 
 // The following arrow function is broken. Can you fix it? (Don't delete the curly braces).
 
-const divide = (a, b) => a / b 
+/*const divide = (a, b) => a / b 
 
 console.log(divide(10,2))
-console.log(divide(18,3))
+console.log(divide(18,3))*/
+
+// Simplify the following arrow function to its shortest form (1 single line), and use a ternary operator.
+
+/*const checkPositive = (n) =>  n > 0 ? true : false 
+
+console.log(checkPositive(20))
+console.log(checkPositive(-5))*/
+
