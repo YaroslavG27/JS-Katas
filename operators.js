c76fc08f5300f843ad825402ccb7fa3c66d9e6b3
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 10:23:47 +0700

    function decrement value works

--- operators.js
+++ operators.js
@@ -2,3 +2,15 @@
 // This function should take a single parameter, a number, and use the decrement operator to decrease the value of the number by one.
 // The function should then return the decremented value.
 
+/*function decrementValue(num){
+    let decrease = num
+    decrease--
+    return decrease
+}
+
+console.log(decrementValue(5))
+console.log(decrementValue(100))*/
+
+
+// Replace the sum calculation in the following code with the Addition Assignment operator.
+// Don't delete the variable c.
