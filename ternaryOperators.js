4ccfdb18699673c2c5e5d81a377d8d4fc2881b01
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 10:41:54 +0700

    create number to boolean function use ternary operator

---
+++ ternaryOperators.js
@@ -0,0 +1,8 @@
+// In the following function, convert the if condition into a ternary operator.
+
+function numerToBoolean(n) {
+   return n > 0 ? true : false
+}
+
+console.log(numerToBoolean(5))
+console.log(numerToBoolean(-30))
