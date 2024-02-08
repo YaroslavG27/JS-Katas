e628ae13ad5a9683695bb3846ab1837479e220e4
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 12:58:26 +0700

    timer fixed

--- operators.js
+++ operators.js
@@ -15,11 +15,22 @@ console.log(decrementValue(100))*/
 // Replace the sum calculation in the following code with the Addition Assignment operator.
 // Don't delete the variable c.
 
-function addTen(a) {
+/*function addTen(a) {
     let c = 10
     c += a
     return c
 }
 
 console.log(addTen(5))
-console.log(addTen(10))
+console.log(addTen(10))*/
+
+// Use a shortcut in the following function to replace the increment of i in the loop and the increment of sum.
+
+function timer() {
+    let sum = 0
+    for (let i = 0; i < 10; i++) {
+      sum++
+    }
+    return sum
+  }
+
