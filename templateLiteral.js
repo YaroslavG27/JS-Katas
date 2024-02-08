8769761622a697a2949c7ed3e73a0f3677dbacd7
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 12:07:08 +0700

    convert concatenation into a template literal

---
+++ templateLiteral.js
@@ -0,0 +1,9 @@
+// Convert the following String Concatenation into a Template Literal.
+
+/*function greet(age, city) {
+    return `Hello, I'm ${age} years old and I'm from ${city}!`
+}
+
+console.log(greet(20,'Tokyo'))
+console.log(greet(37,'Bangkok'))*/
+
