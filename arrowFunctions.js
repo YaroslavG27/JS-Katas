c7e42891280a03cc31cddda8f7ba53cde547ff88
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 11:47:46 +0700

    second kata solved

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,3 +1,11 @@
 // Convert the following function into an arrow functio
 
-const add = (a, b) => a + b
+// const add = (a, b) => a + b
+
+// The following arrow function is broken. Can you fix it?
+
+/*const subtract = (a, b) => a - b
+
+console.log(subtract(5,2))
+console.log(subtract(10,1))*/
+
