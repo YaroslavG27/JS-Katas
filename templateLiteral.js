134639bdc925a65a2e9b7867e62b77bcd009b071
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 12:26:47 +0700

    time greet arrow function created

--- templateLiteral.js
+++ templateLiteral.js
@@ -7,3 +7,17 @@
 console.log(greet(20,'Tokyo'))
 console.log(greet(37,'Bangkok'))*/
 
+//Create an arrow function greetTime that takes a parameter time (a number).
+// Inside the function, create a second arrow function checkTime that takes a parameter t (a number).
+// Within the checkTime function, use a ternary operator to check if t is less than 12, if it is, return 'morning', otherwise return 'evening'.
+// Within the greetTime function, return a template literal that includes the invokation of the function checkTime, passing time as a parameter, to then return either of the following strings:
+
+const timeGreet = (time) => {
+    const checkTime = t => t < 12 ? morning = 'Good morning' : evening = 'Good evening'
+    return `${checkTime(time)}`
+}
+
+console.log(timeGreet(10))
+console.log(timeGreet(11))
+console.log(timeGreet(12))
+console.log(timeGreet(14))
