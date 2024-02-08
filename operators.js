083eceed0aa9b8b8162cf3172fff2c4312355bca
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-08 10:18:36 +0700

    create operators file

---
+++ operators.js
@@ -0,0 +1,4 @@
+// Create a JavaScript function called decrementValue.
+// This function should take a single parameter, a number, and use the decrement operator to decrease the value of the number by one.
+// The function should then return the decremented value.
+
