777cf2c1684fd8bc6687844bedefd9a498917830
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-09 12:59:56 +0700

    create string method file and add a return length function

---
+++ stringMethods.js
@@ -0,0 +1,19 @@
+// Create a returnLength function that takes a parameter and returns its length if it's a string. In any other case it returns the type of the parameter.
+
+/*function returnLength(parameter){
+    if(typeof parameter !== 'string'){
+        return typeof parameter
+    } else {
+        return parameter.length
+    }
+}
+
+console.log(returnLength('hello'))
+console.log(returnLength(''))
+console.log(returnLength(10))
+console.log(returnLength([1,2,3]))
+console.log(returnLength(true))*/
+
+// Create a JavaScript function called isLongerThan.
+// This function should take two parameters: a string and a number.
+// It should return true if the length of the string is greater than the given number, and false otherwise.
