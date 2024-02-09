1d5b181184ab5c0099ad82d7ec4816cc1eaac1f3
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-09 10:55:26 +0700

    create a function that return the length of the array

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,7 +1,13 @@
 // Create a function returnLength that takes an array as its only parameter, then returns the length of the array as a number, or 'the array is empty' if there are no elements in the array.
 
+function returnLength(array){
+    if(array.length === 0){
+        return 'the array is empty'
+    } else {
+        return array.length
+    }
+}
 
-
-returnLength([1,2,3,4])
-returnLength(['a'])
-returnLength([])
+console.log(returnLength([1,2,3,4]))
+console.log(returnLength(['a']))
+console.log(returnLength([]))
