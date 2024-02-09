58bcd8a0ff929dc51c9003dbdb4370c0d3fe9a46
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-09 10:49:42 +0700

    create array methods file

---
+++ arrayMethods.js
@@ -0,0 +1,7 @@
+// Create a function returnLength that takes an array as its only parameter, then returns the length of the array as a number, or 'the array is empty' if there are no elements in the array.
+
+
+
+returnLength([1,2,3,4])
+returnLength(['a'])
+returnLength([])
