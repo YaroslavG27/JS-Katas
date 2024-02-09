66fe84742697230a974c81066622048491759073
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-09 13:12:33 +0700

    function is longer than created

--- stringMethods.js
+++ stringMethods.js
@@ -17,3 +17,13 @@ console.log(returnLength(true))*/
 // Create a JavaScript function called isLongerThan.
 // This function should take two parameters: a string and a number.
 // It should return true if the length of the string is greater than the given number, and false otherwise.
+/*
+const isLongerThan = (strng, num) => strng.length > num ? true : false
+
+console.log(isLongerThan('hello',3))
+console.log(isLongerThan('hello',5))
+console.log(isLongerThan('',1))
+console.log(isLongerThan('',0))*/
+
+// Create a JavaScript function named splitStringIntoArray.
+// This function should take two parameters: a string and a delimiter. It should divide the string into an array of substrings, separated by the specified delimiter, and return the resulting array.
