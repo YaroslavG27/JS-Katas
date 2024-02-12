17ca2ae022062391c1ae22ae80f0319f5126d460
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-12 13:17:33 +0700

    create sort names function

--- sortMethod.js
+++ sortMethod.js
@@ -2,8 +2,15 @@
 // The current implementation sorts the array using a for loop.
 // Your task is to refactor this function to use the sort() method instead, for a more efficient and idiomatic approach.
 
-const sortNumbers = numbers => numbers.sort((number, order) => number - order)
+/*const sortNumbers = numbers => numbers.sort((number, order) => number - order)
 
 
 console.log(sortNumbers([6,5,4,1,2,3]))
-console.log(sortNumbers([20,100,-20,0]))
+console.log(sortNumbers([20,100,-20,0]))*/
+
+// Create a sortNames function that takes an array of strings (names) and returns the array sorted alphabetically.
+
+const sortNames = names => names.sort()
+
+console.log(sortNames(['Bob','Alice','David']))
+console.log(sortNames(['Xenia','Xander','Xavia']))
