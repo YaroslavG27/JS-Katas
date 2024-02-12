7f098a27dd01ed8567c8c2761cac524da3171fb6
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-12 12:55:28 +0700

    sortMethod file created

---
+++ sortMethod.js
@@ -0,0 +1,4 @@
+// You are given a function that takes an array of numbers and returns a new array with the numbers sorted in ascending order.
+// The current implementation sorts the array using a for loop.
+// Your task is to refactor this function to use the sort() method instead, for a more efficient and idiomatic approach.
+
