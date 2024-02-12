9eb0ce62f4a8629ac792234103a25186ac4ce305
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-12 10:33:54 +0700

    creat a functio that filter out the negatives

--- filterMethod.js
+++ filterMethod.js
@@ -12,8 +12,17 @@
 //     return evenNumbers;
 // }
 
-const getEvenNumbers = numbers => numbers.filter(number => number % 2 === 0)
+/*const getEvenNumbers = numbers => numbers.filter(number => number % 2 === 0)
 
 console.log(getEvenNumbers([1,2,3,4,5,6]))
 console.log(getEvenNumbers([0,-10,11,-12,13]))
-console.log(getEvenNumbers([]))
+console.log(getEvenNumbers([]))*/
+
+// Write a JavaScript function named filterOutNegativeNumbers.
+// This function should take an array of numbers and return a new array with all negative numbers filtered out, leaving only positive numbers and zeros.
+
+const filterOutNegativeNumbers = numbers => numbers.filter(number => number >= 0)
+
+console.log(filterOutNegativeNumbers([1,2,3]))
+console.log(filterOutNegativeNumbers([1,-2,-3,4]))
+console.log(filterOutNegativeNumbers([-10,-20,-30,-40]))
