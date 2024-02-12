c74e3feec46b9968cc0baae6460b4eb51b212d0a
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-12 13:14:30 +0700

    refactor sort numbers function

--- sortMethod.js
+++ sortMethod.js
@@ -2,3 +2,8 @@
 // The current implementation sorts the array using a for loop.
 // Your task is to refactor this function to use the sort() method instead, for a more efficient and idiomatic approach.
 
+const sortNumbers = numbers => numbers.sort((number, order) => number - order)
+
+
+console.log(sortNumbers([6,5,4,1,2,3]))
+console.log(sortNumbers([20,100,-20,0]))
