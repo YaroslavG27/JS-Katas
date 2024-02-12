33187025d76fc22dac480d9182bd9c9966229883
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-12 10:22:19 +0700

    map method fixed

--- mapMethod.js
+++ mapMethod.js
@@ -26,7 +26,18 @@ console.log(doubleNumbers([]))*/
 // The conversion formula to use is Fahrenheit = Celsius * 9 / 5 + 32.
 // Use the map method.
 
-const convertToFahrenheit = celsius => celsius.map(cel => cel * 9 / 5 + 32)
+/*const convertToFahrenheit = celsius => celsius.map(cel => cel * 9 / 5 + 32)
 
 console.log(convertToFahrenheit([15,40,12,5]))
-console.log(convertToFahrenheit([42,0,-10]))
+console.log(convertToFahrenheit([42,0,-10]))*/
+
+// You are provided with a function that is intended to take an array of numbers and return a new array with each number squared.
+// However, there's an error in how the map() method is currently being used.
+// Your task is to identify the error and correct the code.
+
+function squareNumbers(numbers) {
+   return numbers.map(number => number * number)
+}
+
+console.log(squareNumbers([1,2,3,4]))
+console.log(squareNumbers([9,0]))
