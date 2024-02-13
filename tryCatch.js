c2d6bae9101c5bec04b55ff4ad0e573fde1ca4e2
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-13 10:53:40 +0700

    add try/catch to the function

--- tryCatch.js
+++ tryCatch.js
@@ -7,4 +7,15 @@ const search = async () => {
       console.log('an error occurred', error.message);
     }
   };
-  
+
+
+//   One of the 2 functions getResults() and getPages() returns an error. You don't know which one. Collect any of the 2 errors and log them in the console.
+
+const search = async () => {
+    try {
+        let results = await getResults()
+        let pages = await getPages()
+    } catch (error){
+        console.log('an error occurred', error.message)
+    }
+  }
