fedbe91475bbf6e1d01df2f18b472f95167958de
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-13 10:58:04 +0700

    try/catch kata done

--- tryCatch.js
+++ tryCatch.js
@@ -1,12 +1,12 @@
 // The function getResults() returns an error. Collect the error and log it in the console.
 
-const search = async () => {
+/*const search = async () => {
     try {
       let results = await getResults();
     } catch (error) {
       console.log('an error occurred', error.message);
     }
-  };
+  };*/
 
 
 //   One of the 2 functions getResults() and getPages() returns an error. You don't know which one. Collect any of the 2 errors and log them in the console.
