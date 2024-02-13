2abb5cfe4204497014ec22087f3270f32e072975
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-13 11:09:22 +0700

    create first to do

---
+++ axios.js
@@ -0,0 +1,22 @@
+// Create an async function getFirstTodo that uses axios to make a request to the url https://jsonplaceholder.typicode.com/todos/1 and returns the data of the response object.
+// Test your code on the Simulation Platform.
+
+/*const getFirstTodo = async () => {
+    let response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    return response.data
+  }
+  
+  console.log(await getFirstTodo())
+
+getFirstTodo() */
+// { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+
+// Create an async function getTodo that:
+
+// receives a parameter number
+// uses axios to make a request to the url https://jsonplaceholder.typicode.com/todos/1 replacing the last 1 with the parameter function
+// returns the data of the response object
+// Test your code on the Simulation Platform.
+
+// Test the following:
+
