d9d5815534826d1dcb55904d5b6b04a8faf95fa7
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-13 10:32:19 +0700

    await added to get message function

--- asyncAwait.js
+++ asyncAwait.js
@@ -2,3 +2,10 @@
 
 const add = async (a, b) => { await a + b }
 console.log(add(2,3))
+
+// The function getMessages reads data from a different server. Write the correct code so that the function waits for it before continuing.
+
+const search = async () => {
+    let messages = await getMessages();
+    return messages;
+  };
